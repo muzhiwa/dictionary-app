42f66d3ae62f09d842668b1ae8d9b37f98a8c1c7
muzhiwa <muzhdawafa2008@gmail.com>
2025-08-14 00:45:57 +0430

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <>
-      <div className='text-center'>
-        Hello World
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Muzhda Wafa</small>
+        </footer>
       </div>
-    </>
+    </div>
   );
 }
-
