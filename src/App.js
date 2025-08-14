478f161bf91835af241cbb84f7b9dec05e52a097
muzhiwa <muzhdawafa2008@gmail.com>
2025-08-15 00:52:12 +0430

    add styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="Sky" />
         </main>
         <footer className="App-footer">
           <small>Coded by Muzhda Wafa</small>
